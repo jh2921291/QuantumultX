158317debd6844e9506b3d2824939d88cda72f96
Orz-3 <61905744+Orz-3@users.noreply.github.com>
2020-03-18 14:00:49 +0800

    Update IP-API.js

--- IP-API.js
+++ IP-API.js
@@ -43,5 +43,5 @@ var obj = JSON.parse(body);
 var title =flags.get(obj['countryCode']) + ' '+ Area_check(obj['country']);
 var subtitle =''+City_ValidCheck(obj['city'])+'-'+'('+ ISP_ValidCheck(obj['org'])+')'+'';
 var ip = obj['query'];
-var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
+var description = '地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] ;
 $done({title, subtitle, ip, description});
