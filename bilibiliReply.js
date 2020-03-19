28cb652ac86cc736d99aac2fc61e0eb3b13e9130
Orz-3 <61905744+Orz-3@users.noreply.github.com>
2020-03-20 02:41:38 +0800

    Add files via upload

--- bilibiliReply.js
+++ bilibiliReply.js
@@ -11,14 +11,8 @@ mitm = ap*.bilibili.com
 
 by onewayticket255
 */
-
 let body = $response.body
 body=JSON.parse(body)
-body['data']['relates'].forEach((element, index)=> {
-   if(element.hasOwnProperty('is_ad')||!element.hasOwnProperty('aid')){      
-      body['data']['relates'].splice(index,1)  
-    }
-})
-delete body['data']['cms']
+delete body['data']['notice']
 body=JSON.stringify(body)
 $done({body})
